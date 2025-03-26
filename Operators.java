e82b09d063389d5b70d6b74e42f7ab115d149399
Sakshi bagul <bagulsakshi29@gmail.com>
2025-03-26 18:31:32 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,31 @@
+public class Operators {
+    public static void main(String[] args) {
+
+
+        int A =10;
+        int B=20;
+
+        // Arithmetic operators
+        // Binary operators
+        // System.out.println("Sum="+ (A+B));
+        // System.out.println("Sub="+ (A-B));
+        // System.out.println("Mul="+ (A*B));
+        // System.out.println("Div="+ (A/B));
+        // System.out.println("Module="+ (A%B));
+
+        // unary operator 
+        // A; // Same ++A;
+        // System.out.println(A);// here value of a will be automatically changed 
+        // System.out.println(A++); 
+
+        // --A;
+        // System.out.println(A); // 9
+        // System.out.println(--A); //8
+        A--;
+        System.out.println(A);//9
+        System.out.println(A--);//9
+
+
+
+    }
+}
