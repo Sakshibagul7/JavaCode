6f866905171e3776d53c444e0ec9ad14083ded47
Sakshi bagul <bagulsakshi29@gmail.com>
2025-03-27 19:56:39 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,116 @@
+
+import java.util.*;
+public class Loops {
+    public static void main(String [] args){
+
+        // While loop
+        // int counter=0;
+        // while(counter<10){
+        //     System.out.println("Hello World");
+        //     counter++;
+
+
+
+        // print numbers from 1 to 10
+        // int num=1;
+        // while(num<11){
+        //     System.out.println(num);
+        //     num++;
+        // }
+
+
+         // print numbers from 1 to n
+
+        //  Scanner sc =new Scanner(System.in);
+        //  System.out.println("Enter n");
+        //  int n =sc.nextInt();
+        //  int a=0;
+        //  while(a<n){
+        //     System.out.println(a);
+        //     a++;
+        //  }
+
+        // Sum of first n natural numbers
+
+        // System.out.println("Enter n");
+        //      int n =sc.nextInt();
+        //      int sum=0;
+        //      int i=0;
+        //      while(i<=n){
+        //         sum=sum+i;
+        //         i++;
+        //      }
+        //      System.out.println(sum);
+        
+        // For loop
+
+        // print a square pattern
+
+        // for(int i=0;i<4;i++){
+        //         System.out.println("****");
+            
+        // }
+
+
+        // Reverse a number 
+        // int number =47858542;
+        // while(number>0){
+        //     int lastDigit=number%10;
+        //     System.out.print(lastDigit);
+        //    number=number/10;
+        // }
+        // System.out.println();
+
+        // do while loop
+
+        // int count =0;
+        // do{
+        //     System.out.println("HI");
+        //     count++;
+        // }while(count<10);
+
+
+        // break statement
+
+        // for(int i=0;i<10;i++){
+        //     if(i>=3){
+        //         break;
+        //     }
+        //  System.out.println(i);
+        // }
+
+
+        // Keep Entering number till user enters multiple of 10 
+
+         Scanner sc =new Scanner(System.in);
+
+        // do{
+        //     System.out.println("Enter your number");
+        //     int n=sc.nextInt();
+        //     if(n%10==0){
+        //         break;
+        //     }
+        //     System.out.println(n);
+        // }while(true);
+
+
+        // Continue statement
+
+
+        // print all numbers entered by user except multiple of 10
+        // do { 
+        //     System.out.println("Enter your number");
+        //     int n=sc.nextInt();
+        //     if(n%10==0){
+        //        continue;
+        //       }
+        //    System.out.println(n);
+        // } while (true);
+
+
+
+    
+
+    }
+    }
+
