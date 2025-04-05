6029fdf71a5f4d6bde5eeeed98b5210d67b8196c
Sakshi bagul <bagulsakshi29@gmail.com>
2025-04-05 22:31:05 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,67 @@
+public class Patterns{
+    public static void main(String[] args) {
+
+
+        // *
+        // **
+        // ***
+        // ****
+        
+        // for(int line=1;line<=4;line++){
+
+        //     for(int star=1; star<=line ;star++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        //Inverted star pattern
+        // ****
+        // ***
+        // **
+        // *
+        
+        //  for(int line=1; line<=4; line++){
+        //     for(int star=1; star<=4-line+1; star++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        //  }
+
+
+
+    //     1
+    //     12
+    //     123
+    //     1234
+    //     12345
+    //     123456
+    //     int n=6;
+    //     for(int line =1;line<=n;line++){
+    //         for(int num=1;num<=line;num++){
+    //             System.out.print(num);
+    //         }
+    //         System.out.println();
+    //     }
+
+
+    // A
+    // BC
+    // DEF
+    // GHIJ
+    // KLMNO
+
+    //  int n=5;
+    //  char ch='A';
+    //  for(int line=1;line<=n;line++){
+    //     for(int chars=1;chars<=line;chars++){
+    //         System.out.print(ch);
+    //         ch++;
+    //     }
+    //     System.out.println();
+    //  }
+
+    
+    }
+}
