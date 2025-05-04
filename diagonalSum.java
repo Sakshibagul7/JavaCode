80baa89776f4cf079fb2dfee0a5567dfc2938c1f
Sakshi bagul <bagulsakshi29@gmail.com>
2025-05-04 11:49:26 +0530

    Add files via upload

--- diagonalSum.java
+++ diagonalSum.java
@@ -1,13 +1,25 @@
 public class diagonalSum {
     public static void addDiagonal(int [][] matrix){
         int sum =0;
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(i==j || i+j==matrix.length -1){
-                    sum+=matrix[i] [j];
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(i==j || i+j==matrix.length -1){
+        //             sum+=matrix[i] [j];
+        //         }
+        //     }
+        // }
+
+
+         // Optimised apporach
+         for(int i=0;i<matrix.length;i++){
+            if(i==i){
+                sum+=matrix[i][i];
+                if(i!=matrix.length-1-i ){
+                   sum+=matrix[i][matrix.length-1-i];
                 }
             }
         }
+         
         System.out.println(sum);
     }
     public static void main(String [] args){
